4496cfa0b9f3aa08eea58fc2d883376ae30da0f1
David Tolnay <dtolnay@gmail.com>
2019-10-31 00:43:11 -0700

    Don't need utf8 check in build.rs

--- build.rs
+++ build.rs
@@ -1,7 +1,7 @@
 fn main() {
     println!("cargo:rerun-if-env-changed=WATT_JIT");
 
-    if std::env::var("WATT_JIT").is_ok() {
+    if std::env::var_os("WATT_JIT").is_some() {
         println!("cargo:rustc-cfg=jit");
     }
 }
